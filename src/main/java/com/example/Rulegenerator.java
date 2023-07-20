f56e0b42f28ceed174a12a97085660f78c0e58ac
AMikail0402 <alpay.yildiz@outlook.com>
2023-07-20 14:36:00 +0200

    erstes Konzept

---
+++ src/main/java/com/example/Rulegenerator.java
@@ -0,0 +1,14 @@
+package com.example;
+public class Rulegenerator {
+    
+    public String rule;
+
+    public String searchPattern;
+
+    public static String ruleGenerator(){
+
+
+        return "";
+    }
+
+}
